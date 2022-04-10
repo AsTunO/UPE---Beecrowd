ea0e5d57c2b5bded792d1cdf50adc0fc6fd3d7a7
Júlio Cesar <juliocesar27072004@outlook.com>
2022-04-10 09:50:22 -0300

    Update 1006.py

--- 1006.py
+++ 1006.py
@@ -6,3 +6,4 @@ if( A not in range(0,11) or B not in range(0,11) or C not in range(0,11)):
 else:
     MEDIA = ((A.__round__(1) * 2) + (B.__round__(1) * 3) + (C.__round__(1) * 5)) / 10
     print("MEDIA = %.1f" % MEDIA)
+    
